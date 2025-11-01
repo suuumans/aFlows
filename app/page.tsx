ca94ba924776fbf9c528b743ef8463688507c78b
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-11-01 22:51:01 +0530

    feat(auth): implement login form with validation and submission handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,21 @@
+'use client'
 
-import { caller } from "@/trpc/server";
-import { getQueryClient } from "@/trpc/server";
 import { Button } from "@/components/ui/button";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+import { authClient } from "@/lib/auth-client";
 
-const page = async () => {
-  const queryClient = getQueryClient();
+const page = () => {
+  const { data } = authClient.useSession();
+  const session = data?.user
+  console.log(session)
 
   return (
     <div className="text-3xl text-amber-100 font-bold bg-black h-screen w-screen px-3 py-3.5">
-      <h1>
-        Hello, world! 👋 🌍 this is aFlows. Where you can create and manage your
-        own agentic workflows.
-      </h1>
-      <div className="text-orange-500">
-        <HydrationBoundary state={dehydrate(queryClient)}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Client />
-          </Suspense>
-        </HydrationBoundary>
-      </div>
-      <div className="mt-10 flex flex-col items-center justify-center gap-4">
-        <Button variant="outline" className="font-bold bg-amber-500">
-          Create Workflow
+      This is the data {JSON.stringify(data, null, 2)}
+      {data && (
+        <Button onClick={() => authClient.signOut()}>
+          Logout
         </Button>
-      </div>
+      )}
     </div>
   );
 };
