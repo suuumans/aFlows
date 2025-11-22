1623453746518ea881c76c358ffeeb0dd2dfe4e4
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-11-22 13:09:37 +0530

    feat: Implement workflow editor with new UI components for breadcrumbs, header, and editable name, and integrate into the workflow detail page.

--- app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,19 +1,37 @@
+
 import { requireAuth } from "@/lib/auth-utils";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import { WorkflowsEditor, WorkflowsEditorError, WorkflowsEditorLoading } from "@/features/workflowsEditor/components/workflowsEditor";
+import { WorkflowsEditorHeader } from "@/features/workflowsEditor/components/workflowsEditorHeader";
 
 interface PageProps {
-  params: Promise <{
+  params: Promise<{
     workflowId: string
   }>
 }
 
 const Page = async ({ params }: PageProps) => {
+
   await requireAuth()
   const { workflowId } = await params;
+  prefetchWorkflow(workflowId)
 
   return (
-    <div>
-      <h1>workflow id: {workflowId}</h1>
-    </div>
+    <>
+      <HydrateClient>
+        <ErrorBoundary fallback={<WorkflowsEditorError />}>
+          <Suspense fallback={<WorkflowsEditorLoading />}>
+            <WorkflowsEditorHeader workflowId={workflowId} />
+            <main className="flex-1">
+              <WorkflowsEditor workflowId={workflowId} />
+            </main>
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </>
   )
 }
 
