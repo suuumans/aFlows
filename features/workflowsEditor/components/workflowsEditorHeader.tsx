8ed4ce70b49148b6d078de3f95732816b23d3be0
Suman Sarkar <152314759+suumans@users.noreply.github.com>
2025-11-29 08:37:12 +0530

    feat: Implement workflow saving by integrating Jotai to manage the ReactFlow instance.

--- features/workflowsEditor/components/workflowsEditorHeader.tsx
+++ features/workflowsEditor/components/workflowsEditorHeader.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { SaveIcon } from "lucide-react";
 import { useEffect, useState, useRef } from "react";
-import { useSuspenseWorkflow, useUpdateWorkflowName } from "@/features/workflows/hooks/use-workflows";
+import { useSuspenseWorkflow, useUpdateWorkflow, useUpdateWorkflowName } from "@/features/workflows/hooks/use-workflows";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { SidebarTrigger } from "@/components/ui/sidebar";
@@ -14,11 +14,32 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { useAtomValue } from "jotai";
+import { editorAtom } from "../store/atoms";
 
 export const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+
+  const editor = useAtomValue(editorAtom);
+  const saveWorkflow = useUpdateWorkflow();
+
+  const handleSave = () => {
+    if (!editor) {
+      return;
+    }
+    
+    const nodes = editor.getNodes();
+    const edges = editor.getEdges();
+    
+    saveWorkflow.mutate({
+      id: workflowId,
+      nodes,
+      edges,
+    });
+  }
+
   return (
     <div className="ml-auto">
-      <Button size="sm" onClick={() => {}} disabled={false}>
+      <Button size="sm" onClick={handleSave} disabled={saveWorkflow.isPending}>
         <SaveIcon className="size-4" />
         Save
       </Button>
